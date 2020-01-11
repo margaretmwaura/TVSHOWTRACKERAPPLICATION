9a8811921340f5cadfcf2f0d0f0625f7c747e781
Margaret Mwaura <mwauramargaret1@gmail.com>
2020-01-11 15:05:12 +0300

    setting up routes , store and node

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id="app">
+        <div id="app_rou">
+            <div id="app_rou_inner">
+                <router-link to="/show" id="thing">Movies</router-link>
+                <router-link to="/create" id="thingt">Create</router-link>
+                <router-link to="/signup" id="thingtt">Signup</router-link>
+                <router-link to="/Login" id="thingttt">Login</router-link>
+            </div>
+
+        </div>
+<!--        <router-view></router-view>-->
+<!--        <notifications group="foo" />-->
+    </div>
+</template>
+
+<script>
+    import Navbar from "./components/Navbar.vue";
+    export default {
+        name: 'app',
+        components:
+            {
+                Navbar
+            },
+        data (){
+            return {
+
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+<style lang="scss">
+    @import "../foundation/css/app.css";
+    @import "../foundation/css/foundation.css";
+    #app {
+        &_rou
+        {
+            width: 100%;
+            height: 100px;
+            background-color: indianred;
+
+            &_inner
+            {
+                padding-top: 50px;
+                text-align: center;
+
+                #thing
+                {
+                    color: indigo;
+                    margin-right: 50px;
+                    text-decoration: none;
+
+                }
+                #thingt
+                {
+                    color: indigo;
+                    margin-right: 50px;
+                    text-decoration: none;
+                }
+                #thingtt
+                {
+                    color: indigo;
+                    margin-right: 50px;
+                    text-decoration: none;
+                }
+                #thingttt
+                {
+                    color: indigo;
+                    margin-right: 50px;
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
 
-Vue.config.productionTip = false
+import Notifications from 'vue-notification'
+Vue.use(Notifications);
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+
+
+const routes = [
+];
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
 
 new Vue({
   router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
+
